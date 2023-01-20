bb25f3d59f5b644d25246ad2f0219d12c30b88ed
Maria Soledad Higa <maemoto.1983@gmail.com>
2023-01-20 19:54:07 -0300

    corrige error en ruta de importacion

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import CartContext from "../context/Car tContext";
+import CartContext from "../context/CartContext";
 import Form from "./Form";
 import Modal from "./Modal";
 
